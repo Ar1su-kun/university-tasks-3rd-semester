d25f69146b083695b75299a2bdcc1bb8b97544a4
Ar1su-kun <antonovart28@gmail.com>
2025-11-19 19:59:07 +0300

    fixes

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 ## [Task 1](https://github.com/Ar1su-kun/CMC-LMSU-tasks-3rd-semester/blob/master/task1)
 ## [Task 2](https://github.com/Ar1su-kun/CMC-LMSU-tasks-3rd-semester/blob/master/task2)
 ## [Task 3](https://github.com/Ar1su-kun/CMC-LMSU-tasks-3rd-semester/blob/master/task3)
+## [Task 4](https://github.com/Ar1su-kun/CMC-LMSU-tasks-3rd-semester/blob/master/task4)
--- task4/echo.c
+++ task4/echo.c
@@ -1,7 +1,23 @@
 #include<stdio.h>
+#include<string.h>
 
 int main(int argc, char *argv[]){
-    for(int i = 1; i < argc; i++){
-        printf((i!=argc-1)?"%s ":"%s\n", argv[i]);
+    int i = 1;
+    char spec[3] = "%s ";
+    char specEnd[4] = "%s\n";
+    if ((argc > 1) && (argv[1][0] == '-') && (strlen(argv[0]) == 2)){
+        switch (argv[1][1]){
+            case 'n':
+                i++;
+                specEnd[3] = '\0';
+                break;
+            case 's':
+                i++;
+                spec[2] = '\0';
+                break;
+        }
+    }
+    for(; i < argc; i++){
+        printf((i!=argc-1)?spec:specEnd, argv[i]);
     }
 }
