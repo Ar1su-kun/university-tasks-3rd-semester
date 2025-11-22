fc88e9d3f1223922e8b321adc218554c59fe2309
Ar1su-kun <antonovart28@gmail.com>
2025-11-22 07:26:00 +0300

    add cp

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# LMSU-3rd-semester-tasks
+# MSU-3rd-semester-tasks
 ## [Task 1](https://github.com/Ar1su-kun/CMC-LMSU-tasks-3rd-semester/blob/master/task1)
 ## [Task 2](https://github.com/Ar1su-kun/CMC-LMSU-tasks-3rd-semester/blob/master/task2)
 ## [Task 3](https://github.com/Ar1su-kun/CMC-LMSU-tasks-3rd-semester/blob/master/task3)
---
+++ task4/cp.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<unistd.h>
+int main(int argc, char *argv[]){
+    char buf[1024];
+    ssize_t n;
+    struct stat s;
+    if (argc > 3){
+        fprintf(stderr, "Usage: cp file_path new_file_path\n");
+        printf("%d\n", argc);
+        return -1;
+    }
+    int fd1, fd2;
+    if ((fd1 = open(argv[1], O_RDONLY)) == -1){
+        fprintf(stderr, "Error: can't open the file %s\n", argv[1]);
+        return -1;
+    }
+    stat(argv[1], &s);
+    if((fd2 = creat(argv[2], s.st_mode)) == -1){
+        fprintf(stderr, "Error: can't create the file %s\n", argv[2]);
+        return -1;
+    }
+    while ((n = read(fd1, buf, sizeof(buf))) > 0)
+        write(fd2, buf, n);
+    return 0;
+}
