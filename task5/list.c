8b32d7212160f5eb36e3b2d6cd1af177ff28ff21
Ar1su-kun <antonovart28@gmail.com>
2025-12-19 18:04:41 +0300

    delete hardcode in /usr/bin/bash insert system

--- task5/list.c
+++ task5/list.c
@@ -1,19 +1,33 @@
 #include "list.h"
 #include<string.h>
 #include "input.h"
+#include<limits.h>
 
-static void initList(list *lst){
+char *varHOME;
+char varSHELL[PATH_MAX];
+char *varUSER;
+char varEUID[20];
+
+void getEnv(){
+    varHOME = getenv("HOME");
+    ssize_t len = readlink("/proc/self/exe", varSHELL, sizeof(varSHELL) - 1);
+    varSHELL[len] = '\0';
+    varUSER = getlogin();
+    sprintf(varEUID, "%d", getuid());
+}
+
+void initList(list *lst){
     lst->size = 0;
     lst->capacity = 0;
     lst->data = NULL;
 }
-static void initBuffer(buffer *buf){
+void initBuffer(buffer *buf){
     buf->size = 0;
     buf->capacity = 0;
     buf->data = NULL;
 }
 
-static void clearList(list *lst){
+void clearList(list *lst){
     if (lst->data == NULL) 
         return;
     for (int i = 0; i < lst->size; i++)
@@ -24,33 +38,33 @@ static void clearList(list *lst){
     lst = NULL;
 }
 
-static void clearBuffer(buffer *buf){
+void clearBuffer(buffer *buf){
     buf->size = 0;
     buf->capacity = 0;
     buf->data = NULL;
 }
 
-static void addSymbol(buffer *buf, int c){
+void addSymbol(buffer *buf, int c){
     if(buf->size >= buf->capacity)
         buf->data = realloc(buf->data, buf->capacity += ADD_CAPACITY);
 
     buf->data[buf->size++] = c;
 }
 
-static void shrinkBuffer(buffer *buf){
+void shrinkBuffer(buffer *buf){
     buf->capacity = buf->size + 1;
     buf->data = realloc(buf->data, buf->capacity);
     buf->data[buf->size++] = '\0';
 }
 
-static void shrinkList(list *lst){
+void shrinkList(list *lst){
     if(lst->data != NULL){
         lst->data = realloc(lst->data, lst->size * sizeof(lst->data));
         lst->capacity = lst->size;
     }
 }
 
-static void addWord(list *lst, buffer *buf){
+void addWord(list *lst, buffer *buf){
         shrinkBuffer(buf);
 
         if (lst->size >= lst->capacity)
@@ -59,14 +73,76 @@ static void addWord(list *lst, buffer *buf){
         lst->data[lst->size++] = buf->data;
 }
 
-static void printSize(list *lst){
+void printSize(list *lst){
     printf("Size: %d\n", lst->size);
 }
 
-static void printList(list *lst){
+void printList(list *lst){
     if (lst->data == NULL)
         return;
     printf("[");
     for (int i = 0; i < lst->size-1; i++)
         printf("%s, ", lst->data[i]);
+    printf("%s]\n", lst->data[lst->size - 1]);
 }
+
+void insertVariables(list *lst){
+    if (lst->data == NULL)
+        return;
+
+    list temp;
+    buffer buf;
+    initBuffer(&buf);
+    initList(&temp);
+    getEnv();
+
+
+    for (int i = 0; i < lst->size; i++){
+        clearBuffer(&buf);
+        if((lst->data[i][0] != '$')){
+            for (int j = 0; j < strlen(lst->data[i]); j++){
+                addSymbol(&buf, lst->data[i][j]);
+            }
+            addWord(&temp, &buf);
+        }
+        else if(!strcmp(lst->data[i], "$HOME")){
+            for(int j = 0; j < strlen(varHOME); j++){
+                addSymbol(&buf, varHOME[j]);
+            }
+            addWord(&temp, &buf);
+        }
+        else if(!strcmp(lst->data[i], "$SHELL")){
+            for(int j = 0; j < strlen(varSHELL); j++){
+                addSymbol(&buf, varSHELL[j]);
+            }
+            addWord(&temp, &buf);
+        } 
+        else if(!strcmp(lst->data[i], "$USER")){
+            for(int j = 0; j < strlen(varUSER); j++){
+                addSymbol(&buf, varUSER[j]);
+            }
+            addWord(&temp, &buf);
+        }
+        else if (!strcmp(lst->data[i], "$EUID")){
+            for(int j = 0; j < strlen(varEUID); j++){
+                addSymbol(&buf, varEUID[j]);
+            }
+            addWord(&temp, &buf);
+        }
+    }
+
+    shrinkList(&temp);
+    clearList(lst);
+    *lst = temp;
+}
+
+
+
+
+
+
+
+
+
+
+
